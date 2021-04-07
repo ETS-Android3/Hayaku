760e91142191be0473de9649e9fcc5991c167b6d
Yangjin Cho <7033942cyj@dongguk.edu>
2021-04-07 12:08:01 +0900

    Implemented Firebase

--- Hayaku/app/src/main/java/com/riel_dev/hayaku/GetTimelineService.java
+++ Hayaku/app/src/main/java/com/riel_dev/hayaku/GetTimelineService.java
@@ -1,12 +1,26 @@
 package com.riel_dev.hayaku;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.RemoteInput;
 
+import java.util.Random;
+
+import twitter4j.ResponseList;
+import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
@@ -46,6 +60,16 @@ public class GetTimelineService extends Service {
             twitterFactory = new TwitterFactory(configurationBuilder.build());
             twitter = twitterFactory.getInstance();
         }
+
+        try {
+            ResponseList<Status> timeline = twitter.getHomeTimeline();
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+
+
+
         return super.onStartCommand(intent, flags, startId);
     }
+
 }
